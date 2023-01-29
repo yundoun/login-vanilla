4530cfd057aec942c0b8b740c3373be4bfa07b28
doun <dbsehdns12@gmail.com>
2023-01-29 13:34:39 +0900

    js 파일 추가해보기

---
+++ main.js
@@ -0,0 +1,7 @@
+const $loginSection = document.getElementsByClassName('login')[0]
+
+const $loginTitle = document.createElement('h2')
+
+$loginTitle.innerText = '로그인'
+
+$loginSection.appendChild($loginTitle)
